d2545850317cbaaf72e90b7181c90310d011dcaa
Carmen-Git-It <carmenwhitton@gmail.com>
2022-04-29 14:47:54 -0400

    Added unix timestamp api response

--- server.js
+++ server.js
@@ -24,7 +24,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:date", (res, req) => {
+  let date = new Date(req.params.date);
+  if (isNaN(date)) {
+    // Date is invalid
+  } else {
+    res.send({"unix": date.getTime().toString()});
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
